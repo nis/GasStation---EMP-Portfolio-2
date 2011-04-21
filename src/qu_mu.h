b0eda1242d24e9d09fc7cfa7def2f51c1793d95a
Nis Sarup <nis@sarup.dk>
2011-04-21 17:20:30 +0200

    Added to hold mutexes and queues. They are still defined in main.c

---
+++ src/qu_mu.h
@@ -0,0 +1,38 @@
+/*****************************************************************************
+* University of Southern Denmark
+* Embedded Programming (EMP)
+*
+* MODULENAME.: emp.h
+*
+* PROJECT....: EMP
+*
+* DESCRIPTION: Test.
+*
+* Change Log:
+******************************************************************************
+* Date    Id    Change
+* 
+* --------------------
+* 30/03/2011 NS    Module created.
+*
+*****************************************************************************/
+
+#ifndef QU_MU_H_
+#define QU_MU_H_
+
+/***************************** Include files *******************************/
+
+/*****************************    Defines    *******************************/
+
+// all mutex used in this c program
+extern xSemaphoreHandle lcd_buffer_mutex;
+extern xSemaphoreHandle lcd_keyboard_port_mutex;
+
+// all queues in this c code
+
+/*****************************   Constants   *******************************/
+
+/*****************************   Functions   *******************************/
+
+/****************************** End Of Module *******************************/
+#endif
