c7b412930559f75532314d6495ff8227a688798e
Nis Sarup <nis@sarup.dk>
2011-04-23 17:49:08 +0200

    Added events, queues and mutexes.

--- src/defines.h
+++ src/defines.h
@@ -24,12 +24,45 @@
 
 /*****************************    Defines    *******************************/
 
+// Events
+	// Money events
+	#define ADD_50 						0
+	#define ADD_100 					1
+	#define FROM_ACCOUNT 				2
+	#define ACCOUNT_NUMBER_ENTERED		4
+	#define ACCOUNT_PIN_ENTERED			5
+	
+	// Choosing product
+	#define PRODUCT_BUTTON_CLICKED	 	6
+	
+	// Filling
+	#define HANDLE_LIFTED			 	7
+	#define HANDLE_REPLACED 			8
+	#define MONEY_USED					9
+	
+	// Timeouts
+	#define BUY_TIMEOUT 				10
+	
+	// Administrative events
+	#define UART_SET_PRICE 				11
+	#define UART_GET_REPORT 			12
+
 // Products
 #define OCTANE_92  0
 #define OCTANE_95  1
 #define OCTANE_85  2
 
 /*****************************   Constants   *******************************/
+
+/*****************************   Variables   *******************************/
+
+// all mutex used in this c program
+extern xSemaphoreHandle lcd_buffer_mutex;
+extern xSemaphoreHandle lcd_keyboard_port_mutex;
+
+// all queues in this c code
+extern xQueueHandle uart_output_queue;
+extern xQueueHandle uart_input_queue;
 
 /*****************************   Functions   *******************************/
 
