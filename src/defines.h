254b04861de896ab7439b00cc6d2987b0f427695
Nis Sarup <nis@sarup.dk>
2011-04-24 09:16:39 +0200

    Added states, structs and events.

--- src/defines.h
+++ src/defines.h
@@ -24,6 +24,14 @@
 
 /*****************************    Defines    *******************************/
 
+// Pump states
+	#define PUMP_IDLE 					0
+	#define PUMP_SLOW 					1
+	#define PUMP_RAMP_UP 				2
+	#define PUMP_SS 					3
+	#define PUMP_RAMP_DOWN 				4
+	#define PUMP_TARGET_REACHED 		5
+
 // Events
 	// Money events
 	#define ADD_50 						0
@@ -49,13 +57,46 @@
 	#define UART_GET_REPORT 			13
 
 // Products
-#define OCTANE_92  0
-#define OCTANE_95  1
-#define OCTANE_85  2
+	#define OCTANE_92  					0
+	#define OCTANE_95  					1
+	#define OCTANE_85  					2
+	
+// UART commands
+	#define UART_SET_PRICE				0
+	#define UART_GET_REPORT				1
+	
+// Gasstation events
+	#define EVENT_ACCOUNT_CLICK			0
+	#define EVENT_ADD_50_KR				1
+	#define EVENT_ADD_100_KR			2
+	#define EVENT_ACCOUNT_RECEIVED		3
+	#define EVENT_PIN_RECEIVED			4
+	#define EVENT_PRODUCT_CLICK			5
+	#define EVENT_HANDLE_LIFTET			6
+	#define EVENT_CLOSE_TO_TARGET		7
+	#define EVENT_LITTLE_TIMEOUT		8 // Determines when to jump from 40% to ramp up while pumping
+	#define EVENT_BIG_TIMEOUT			9 // 15sec "global" timeout
+	#define EVENT_PUMP_AT_100			10
+	#define EVENT_HANDLE_REPLACED		11
+	#define EVENT_RECEIPT_TIMEOUT		12 // How long should the receipt be shown?
+	
 
 /*****************************   Constants   *******************************/
 
 /*****************************   Variables   *******************************/
+
+// Structures
+struct uart_command
+		{
+			INT8U command;	// From the list of defines above
+			INT8U product;	// Only used when setting price
+			INT16U price;	// Only used when setting price
+		};
+		
+struct gasstation_event		// This might not need to be a struct
+		{
+			INT8U event;
+		};
 
 // all mutex used in this c program
 extern xSemaphoreHandle lcd_buffer_mutex;
